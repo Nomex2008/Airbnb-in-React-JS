37f8acee2386d07449c5950f53ec3df3a733b071
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-11-25 00:28:13 +0200

    Commit 1(Header1)
    Add start part of header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Logo from '../Logo/Logo'
+import SearchBar from '../SearchBar/SearchBar'
 import styles from './styles.module.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+        <div className="container">
+            <div className={styles.header__row}>
+                <div className={styles.header__logo}><Logo/></div>
+                <div className={styles.header__search}><SearchBar/></div>
+                <div className={styles.header__user}>user menu</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
