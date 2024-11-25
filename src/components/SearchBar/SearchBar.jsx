ec0c54a3186c7b9416511de704e245d9043646b9
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-11-25 23:59:14 +0200

    Commit 2(Header2)

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import styles from './styles.module.scss'
+import React from 'react';
+import styles from './styles.module.scss';
+import searchIcon from './search.svg';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div className={styles.searchbar}>
+      <div className={styles['searchbar__btns-wrapper']}>
+        <button className={styles.searchbar__btn}>Anywhere</button>
+        <button className={styles.searchbar__btn}>Any week</button>
+        <button className={styles.searchbar__btn}>Add guests</button>
+      </div>
+      <button className={styles['searchbar__btn-search']}>
+        <img src={searchIcon} alt="Search" />
+      </button>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
