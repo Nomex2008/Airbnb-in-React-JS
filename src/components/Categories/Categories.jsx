944f5366d1c5e8c0286d234595ce88b7e3a2144e
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2025-01-16 00:23:03 +0200

    Commit 10(CategoryItem3)
    Add items

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,13 +2,26 @@ import React from 'react'
 import CategoryItem from '../CategoryItem/CategoryItem'
 import FilterBtn from '../FilterBtn/FilterBtn'
 import styles from './styles.module.scss'
+import categoriesData from './../../data/categoriesData.json'
 
 const Categories = () => {
+  //console.log(categoriesData)
+  
   return (
     <div className={styles.categories}>
         <div className="container">
             <div className={styles.categories__row}>
-                <div className={styles.categories__list}><CategoryItem/></div>
+                <div className={styles.categories__list}>
+                  {categoriesData.map((cat) => {
+                    return(
+                      <CategoryItem
+                      key={cat.title}
+                      title={cat.title}
+                      img={cat.img}
+                      />
+                    )
+                  })}
+                  </div>
                 <div className={styles.categories__filter}>
                   <FilterBtn/>
                 </div>
--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
-const CategoryItem = () => {
+const CategoryItem = ({title, img}) => {
   return (
     <div className={styles.wrapper}>
         <div className={styles.img}>
-            <img src="img/categories/01.svg" alt="Cat name" />
+            <img src={`img/categories/${img}`} alt="Cat name" />
         </div>
-        <div className={styles.title}>Category name</div>
+        <div className={styles.title}>{title}</div>
     </div>
   )
 }
 
-export default CategoryItem
+export default CategoryItem;
