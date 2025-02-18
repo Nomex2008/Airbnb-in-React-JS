427fde726028089379c4b3cb2365fb33bfc2e87a
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2025-02-19 00:43:37 +0200

    Commit 16(Footer1)
    Start footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,32 @@ import styles from './styles.module.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <div className="container">
+        <div className={styles.footer__body}>
+
+          <ul className={styles.footer__left}>
+            <li className={styles.leftItem}>© 2023 Airbnb, Inc.</li>
+            <li className={styles.leftItem}>Terms</li>
+            <li className={styles.leftItem}>Sitemap</li>
+            <li className={styles.leftItem}>Privacy</li>
+            <li className={styles.leftItem}>Your Privacy Choices</li>
+          </ul>
+
+          <ul className={styles.footer__right}>
+            <li className={styles.rightItem}>
+              
+              English (US)</li>
+            <li className={styles.rightItem}>USD</li>
+            <li className={styles.rightItem}>Support & resources</li>
+            <li className={styles.rightItem}>
+
+            </li>
+          </ul>
+
+          </div>
+      </div>
+    </div>
   )
 }
 
